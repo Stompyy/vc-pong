e3ea0fef870a370216b9e956613f95bf045a1443
Richard Steele <richvon2208@googlemail.com>
2016-10-11 14:06:14 +0100

    qwerty

---
+++ Pong/main.py
@@ -0,0 +1,13 @@
+import pygame, sys, time, random
+from pygame.locals import *
+
+pygame.init()
+pygame.mixer.init()
+
+
+
+class ball
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.image = 
