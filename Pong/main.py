de13cd66d4f372592fb2022e7f5f85657bd0ba73
Richard Steele <richvon2208@googlemail.com>
2016-10-11 14:36:21 +0100

    update 0.0.0.01

--- Pong/main.py
+++ Pong/main.py
@@ -4,14 +4,34 @@ from pygame.locals import *
 pygame.init()
 pygame.mixer.init()
 
+# Declare all variables etc
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+height = 600
+width = 800
+
+window = pygame.display.set_mode((width, height), 0, 32)
 
 
-class ball
+
+class ball:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.image =
+        # self.image = pygame.draw.
 
-    def wallBounce(self):
+    def wallBounce(self, incomingY):
+        self.y = - incomingY
+        return self.y
 
-        
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
